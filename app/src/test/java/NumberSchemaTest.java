aa09077a9968bfd85e5e00d82c90d34da3b06e8f
serge loie <serge.loie@yandex.ru>
2023-11-15 15:32:46 +0500

    rebuild project

--- app/src/test/java/NumberSchemaTest.java
+++ app/src/test/java/NumberSchemaTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
--- app/src/test/java/StringSchemaTest.java
+++ app/src/test/java/StringSchemaTest.java
@@ -3,6 +3,7 @@
  */
 package hexlet.code;
 
+import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
