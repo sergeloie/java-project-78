3c9e3704c8bb6d1ce1adf8192105088ed4871922
hyperadmin <serge.loie@yandex.ru>
2023-11-21 16:57:10 +0500

    beginning of solution

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,12 +1,45 @@
 package hexlet.code.schemas;
 
-public abstract class BaseSchema {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 
+public abstract class BaseSchema<T> {
+
+    public List<Predicate<T>> predicates = new ArrayList<>();
 
 
     /**
      * @param object for validating
      * @return status of validation
      */
-    public abstract boolean isValid(Object object);
+    public boolean isValid(T object) {
+
+        for (Predicate<T> predicate : predicates) {
+            System.out.println(predicate.test(object));
+        }
+
+        return predicates.stream().allMatch(predicate -> predicate.test(object));
+//
+//        if (object == null && predicates.stream().noneMatch(predicate -> predicate == (Predicate<T>) Objects::nonNull)) {
+//            return true;
+//        }
+//
+//        return predicates.stream().allMatch(predicate -> predicate.test(object));
+
+//        if (object == null) {
+//            return predicates.isEmpty() ? true : predicates.stream().anyMatch(predicate -> !predicate.test(null));
+//        }
+//        return predicates.stream().allMatch(predicate -> predicate.test(object));
+    };
+
+    public void addPredicate(Predicate<T> predicate) {
+        predicates.add(predicate);
+    }
+
+    public boolean containsNullCheck() {
+        return predicates.stream().anyMatch(predicate -> predicate.test(null));
+        };
+
 }
--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,45 +1,24 @@
 package hexlet.code.schemas;
 
-public final class NumberSchema extends BaseSchema {
+import java.util.Objects;
 
-    private boolean required = false;
-    private boolean positive = false;
-    private Integer min = Integer.MIN_VALUE;
-    private Integer max = Integer.MAX_VALUE;
+public final class NumberSchema extends BaseSchema<Integer> {
+
 
     public NumberSchema required() {
-        this.required = true;
+        addPredicate(x -> x != null);
         return this;
     }
 
     public NumberSchema positive() {
-        this.positive = true;
+        addPredicate(i -> i > 0);
         return this;
     }
 
     public NumberSchema range(int a, int b) {
-        this.min = a;
-        this.max = b;
+        addPredicate(i -> (i >= a && i <= b));
         return this;
     }
 
-    @Override
-    public boolean isValid(Object object) {
 
-        if (object == null) {
-            return !required;
-        }
-
-        if (!(object instanceof Integer data)) {
-            return false;
-        }
-
-        if (positive && data <= 0) {
-            return false;
-        }
-        if (data < min || data > max) {
-            return false;
-        }
-        return true;
-    }
 }
